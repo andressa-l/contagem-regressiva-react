c05f71e391e7da2c0e11108e912e3a8de9192150
Andressa Lima <gregorio.andressa.lima@gmail.com>
2024-01-12 11:06:52 -0300

    updates datas

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import Span from './components/Title'
 import './App.css'
 
 function App() {
-  const [day, hour, minute, second] = useCountDown('01/01/2024 00:00:00')
+  const [day, hour, minute, second] = useCountDown('01/01/2025 00:00:00')
   return (
     <div className='App' style={{ backgroundImage: `url(${NewYear})` }}>
       <div className='container'>
         <Title title="Happy New Year" />
-        <Span span="2024" />
+        <Span span="2025" />
 
         <div className='contagem-regressiva'>
           <Contador title="Dias" number={day} />
